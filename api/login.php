362fd2c53fd533ac368b2396b83a38eabdef63c2
EMATP <ematp.soporte@caraludme.edu.ar>
2022-10-20 20:08:24 -0300

    Added Backend Class Pseudo-Implementation Example

--- api/login.php
+++ api/login.php
@@ -1,75 +1,33 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
-include_once( "./lib/database.php");
+/**
+ * Copyright (c) 2022 Thiago Cabrera All rights reserved.
+ * Contact: thiagofcabrera00@gmail.com
+ * Released under the GPLv3
+ * https://www.gnu.org/licenses/gpl-3.0
+ **/
 
-
-$json_body = file_get_contents('php://input');
-$object = json_decode($json_body);
-
-$connection = new Connection();
-
-$connection_isft_db = $connection->get_connection_isft_db();
-
-$password = $object->password;
-$username = $object->username;
-
-function createUserSession( $id_user )
-{
-	$token = hash('sha256', $username.$password);
-
-	$SQLStatement = $connection_isft_db->prepare("CALL `create_user_session`(:id_user, :token)");
-	$SQLStatement->bindParam( ':id_user', $id_user );
-	$SQLStatement->bindParam( ':token', $token );
-	$SQLStatement->execute();
-
-	return $token;
-}
+include_once "./lib/auth.php";
 
 try
 {
-	//Todo tipo de validación de información, debe ser realizada aquí de manera obligatoria
-	//ANTES de enviar el comando SQL al motor de base de datos.
-
-	//PRIMER PASO: Validar la existencia del usuario y su contraseña
-	$SQLStatement = $connection_isft_db->prepare("CALL `auth_user`(:username, :password)");
-	$SQLStatement->bindParam( ':username', $username );
-	$SQLStatement->bindParam( ':password', $password );
-	$SQLStatement->execute();
-
-	//Procesar las respuestas posibles del procedimiento almacenado
-	$db_response = $SQLStatement->fetchAll(PDO::FETCH_ASSOC);
-	$SQLStatement->closeCursor();
-
-	$response_client = null;
-
-	if ( count($db_response) != 0 )
-	{
-		//caso favorable (el array no está vacío y se espera un array con la estructura de la respuesta que genera el procedimiento)
-		$id_user = $db_response[0]["id"];
-
-		$token = hash('sha256', $username.$password);
-
-		$SQLStatement2 = $connection_isft_db->prepare("CALL `create_user_session`(:id_user, :token)");
-		$SQLStatement2->bindParam( ':id_user', $id_user );
-		$SQLStatement2->bindParam( ':token', $token );
-		$SQLStatement2->execute();
-		$SQLStatement2->closeCursor();
-
-		$response_client = [ "status" => "OK", "response" => $token ];
-	}
-	else
-	{
-		//caso desfavorable (el array está vacio)
-		$response_client = [ "status" => "ERROR", "description" => 'Usuario y/o Contraseña errónea'];
-	}
-
-	echo json_encode($response_client);
+    //Get Input Data
+	$input = json_decode(file_get_contents('php://input'));
+	$password = $input->password;
+	$username = $input->username;
+	
+	//Check input conditions (patterns, sizes, conditions... )
+    
+    //Execute API Class Method/Procedure
+    $authInstance = new AuthManager();
+    $authInstance->login($username, $password);
+    
+    //Process output
+    
+    //Build Client Response
 }
-catch( PDOException $connectionException )
+catch (Exception $connectionException)
 {
-    $status = array( 'status'=>'db-error ', 'description'=>$connectionException->getMessage() );
-    echo json_encode($status);
-    die();
+    //Handle Possible Errors
+    
+    //Build Error Client Messages
 }
-
-?>
