64fae4e6abfa4a0a592ac31895d9cb37aafb4726
Celeste_A <40323568+celeste-ar@users.noreply.github.com>
2022-10-21 20:06:09 -0300

    Add files via upload

--- view/lib/WCLogInSession/view/appView.js
+++ view/lib/WCLogInSession/view/appView.js
@@ -1,8 +1,8 @@
-
 class AppView extends HTMLElement 
 {
     
-	constructor(){
+	constructor()
+    {
 
         super(); 
         this.container = document.createElement('div');
@@ -45,42 +45,7 @@ class AppView extends HTMLElement
 
     }
 
-    getFormValues()
-    {
-        let values = {
-            username: this.usernameInput.value,
-            password: this.passwordInput.value
-        }
-        return values;
-    }
-    getSessionToken()
-    {
-	    let authData =
-	    {
-		    token: window.sessionStorage.getItem('token')
-	    };
-
-	    return authData;
-    }
-    welcomeView( data )
-    {
-	    if ( data.status == 'OK')
-	    {
-		    alert('Bienvenido al sistema usuario: '+data.response );
-		    window.sessionStorage.setItem('token', data.response );
-	    }else
-	    {
-		    alert('ERROR: '+data.description);
-	    }
-    }
-    onValidateUserButtonClick(data)
-    {
-	    
-	   fetch('./backend/test.php', { method:'post', body: JSON.stringify(this.getFormValues()) } )
-	    .then( response => response.json() )
-	    .then( response => { this.welcomeView(response) } );
-	
-    }
+   
         
 
     connectedCallback(){
