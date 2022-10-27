46b5b2eb941a09635097f8a3e0ad6d79dfaab330
a <=>
2022-10-27 20:25:44 -0300

    Added functionability

--- api/lib/ActionHandler.php
+++ api/lib/ActionHandler.php
@@ -10,23 +10,35 @@
         }
 
         public function create(string $name, string $description){
-            #create action
+            $SQLStatement =$this->connection->prepare("CALL `usp_create_action`(:name, :description)");
+            $SQLStatement->bindParam(':name', $name);
+            $SQLStatement->bindParam(':description', $description);
+            $SQLStatement->execute();
         }
 
         public function remove(int $action_id){
-            #remove action
+            $SQLStatement =$this->connection->prepare("CALL `usp_delete_action`(:id_action)");
+            $SQLStatement->bindParam(':id_action', $action_id);
+            $SQLStatement->execute();
         }
         
         public function update(int $action_id, string $name, string $description){
-            #update action
+            $SQLStatement =$this->connection->prepare("CALL `usp_update_action`(:id_action, :name, :description)");
+            $SQLStatement->bindParam(':id_action', $name);
+            $SQLStatement->bindParam(':name', $description);
+            $SQLStatement->bindParam(':description', $description);
+            $SQLStatement->execute();
         }
         
         public function get(int $action_id){
-            #get action
+            $SQLStatement =$this->connection->prepare("CALL `usp_get_action`(:id_action)");
+            $SQLStatement->bindParam(':id_action', $action_id);
+            $SQLStatement->execute();
         }
         
         public function getAll(){
-            #get all actions
+            $SQLStatement =$this->connection->prepare("CALL `usp_getAll_action`()");
+            $SQLStatement->execute();
         }
 
     };
--- api/lib/UserInformationHandler.php
+++ api/lib/UserInformationHandler.php
@@ -10,23 +10,35 @@
         }
 
         public function create(int $user_id, string $name, string $surname, string $dni, string $email){
-            #create user info
+            $SQLStatement = $this->connection->prepare("CALL `usp_create_user_information`(:name, :description)");
+            $SQLStatement->bindParam(':name', $name);
+            $SQLStatement->bindParam(':description', $description);
+            $SQLStatement->execute();
         }
 
-        public function remove(int $action_id){
-            #remove user info
+        public function remove(int $user_information_id){
+            $SQLStatement =$this->connection->prepare("CALL `usp_delete_user_information`(:id_user_information)");
+            $SQLStatement->bindParam(':id_user_information', $user_information_id);
+            $SQLStatement->execute();
         }
         
         public function update(int $user_id, string $name, string $surname, string $dni, string $email){
-            #update user info
+            $SQLStatement =$this->connection->prepare("CALL `usp_update_user_information`(:id_user_information, :name, :description)");
+            $SQLStatement->bindParam(':id_user_information', $name);
+            $SQLStatement->bindParam(':name', $description);
+            $SQLStatement->bindParam(':description', $description);
+            $SQLStatement->execute();
         }
         
         public function get(int $user_id){
-            #get user info
+            $SQLStatement =$this->connection->prepare("CALL `usp_get_user_information`(:id_user_information)");
+            $SQLStatement->bindParam(':id_user_information', $user_information_id);
+            $SQLStatement->execute();
         }
         
         public function getAll(){
-            #get all actions
+            $SQLStatement =$this->connection->prepare("CALL `usp_getAll_user_information`()");
+            $SQLStatement->execute();
         }
 
     };
