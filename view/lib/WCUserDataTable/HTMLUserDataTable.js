bf9b654073ce815c04f421e06375f336aae6ca69
LucianoFrias <69430221+LucianoFrias@users.noreply.github.com>
2022-11-10 19:32:36 -0300

    WCUserDataTable done. Looking to centralize all files.
    TO-DO:
    
    - Looking to attach all WC files into view/main.js file when all Web Components are finally done.

--- view/lib/WCUserDataTable/HTMLUserDataTable.js
+++ view/lib/WCUserDataTable/HTMLUserDataTable.js
@@ -11,18 +11,21 @@ class HTMLUserDataTable extends HTMLElement
 
         this.ABMDataTable = new HTMLABMDataTableView();
         this.modalCreate = new Modal();
+        this.modalEdit = new Modal();
+        this.modalDelete = new Modal();
     
-        this.nameInput = document.createElement('input');
-        this.nameInput.placeholder = 'Name';
+        // MODAL CREATE CONTENT
+        this.nameInputCreate = document.createElement('input');
+        this.nameInputCreate.placeholder = 'Name';
 
-        this.lastnameInput = document.createElement('input');
-        this.lastnameInput.placeholder = 'Last Name';
+        this.lastnameInputCreate = document.createElement('input');
+        this.lastnameInputCreate.placeholder = 'Last Name';
 
-        this.genderInput = document.createElement('input');
-        this.genderInput.placeholder = 'Gender';
+        this.genderInputCreate = document.createElement('input');
+        this.genderInputCreate.placeholder = 'Gender';
 
-        this.emailInput = document.createElement('input');
-        this.emailInput.placeholder = 'Email';
+        this.emailInputCreate = document.createElement('input');
+        this.emailInputCreate.placeholder = 'Email';
 
         this.addUserButton = document.createElement('button');
         this.addUserButton.innerText = 'Add User to Table';
@@ -30,13 +33,58 @@ class HTMLUserDataTable extends HTMLElement
             this.modalCreate.modal.style.display = 'none';
         })
 
+        // MODAL EDIT CONTENT
+
+        this.nameInputEdit = document.createElement('input');
+        this.nameInputEdit.placeholder = 'Name';
+
+        this.lastnameInputEdit = document.createElement('input');
+        this.lastnameInputEdit.placeholder = 'Last Name';
+
+        this.genderInputEdit = document.createElement('input');
+        this.genderInputEdit.placeholder = 'Gender';
+        
+        this.emailInputEdit = document.createElement('input');
+        this.emailInputEdit.placeholder = 'Email';
+
+        this.editUserButton = document.createElement('button');
+        this.editUserButton.innerText = 'Update User';
+        this.editUserButton.addEventListener('click', () => {
+            this.modalEdit.modal.style.display = 'none';
+        })
+
+        // MODAL DELETE CONTENT
+
+        this.nameInputDelete = document.createElement('input');
+        this.nameInputDelete.placeholder = 'Name';
+
+        this.lastnameInputDelete = document.createElement('input');
+        this.lastnameInputDelete.placeholder = 'Last Name';
+
+        this.emailInputDelete = document.createElement('input');
+        this.emailInputDelete.placeholder = 'Email'
+
+        this.deleteUserButton = document.createElement('button');
+        this.deleteUserButton.innerText = 'Delete User';
+        this.deleteUserButton.addEventListener('click', () => {
+            this.modalDelete.modal.style.display = 'none';
+        })
+
+        // TABLE BUTTONS
+
         this.ABMDataTable.createButton.addEventListener('click', () =>{
             this.modalCreate.modal.style.display = 'block';
         })
-        
-        
-        
-        
+
+        this.ABMDataTable.editButton.addEventListener('click', () => {
+            this.modalEdit.modal.style.display = ' block';
+        })
+
+        this.ABMDataTable.deleteButton.addEventListener('click', () => {
+            this.modalDelete.modal.style.display = 'block';
+        })
+
+
         // Columns
         this.table = this.ABMDataTable.datatable;
         this.table.appendColumn({name:'name', sortable:true, type:HTMLTextCell,	title:"Name", reader: x=>x.name});
@@ -51,11 +99,24 @@ class HTMLUserDataTable extends HTMLElement
     {
         this.appendChild(this.ABMDataTable);
         this.appendChild(this.modalCreate);
-        this.modalCreate.modalContent.appendChild(this.nameInput);
-        this.modalCreate.modalContent.appendChild(this.lastnameInput);
-        this.modalCreate.modalContent.appendChild(this.genderInput);
-        this.modalCreate.modalContent.appendChild(this.emailInput);
+        this.modalCreate.modalContent.appendChild(this.nameInputCreate);
+        this.modalCreate.modalContent.appendChild(this.lastnameInputCreate);
+        this.modalCreate.modalContent.appendChild(this.genderInputCreate);
+        this.modalCreate.modalContent.appendChild(this.emailInputCreate);
         this.modalCreate.modalContent.appendChild(this.addUserButton);
+
+        this.appendChild(this.modalEdit);
+        this.modalEdit.modalContent.appendChild(this.nameInputEdit);
+        this.modalEdit.modalContent.appendChild(this.lastnameInputEdit);
+        this.modalEdit.modalContent.appendChild(this.genderInputEdit);
+        this.modalEdit.modalContent.appendChild(this.emailInputEdit);
+        this.modalEdit.modalContent.appendChild(this.editUserButton);
+
+        this.appendChild(this.modalDelete);
+        this.modalDelete.modalContent.appendChild(this.nameInputDelete);
+        this.modalDelete.modalContent.appendChild(this.lastnameInputDelete);
+        this.modalDelete.modalContent.appendChild(this.emailInputDelete);
+        this.modalDelete.modalContent.appendChild(this.deleteUserButton);
     }
 }
 
